33508cf73b1ed95a15b9260a0510fc03ce039723
DAMILOLA <dammylola299@gmail.com>
2023-11-22 01:07:09 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import AppLogo from './assets/app-logo.png';
+import CGLogo from './assets/open-ai.png';
 
 function App() {
+  const [prompt, setPrompt] = useState('');
+  const [response, setResponse] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // communicate with API...
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <img src={AppLogo} className='app-logo' alt='app-logo' />
+      <form onSubmit={handleSubmit}>
+        <img src={CGLogo} className={ loading ? 'cg-logo loading' : 'cg-logo' } 
+          alt='cg-logo' 
+        />
+        <input
+          type='text'
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+          placeholder='Ask me anything... :)'
+        />
+        <button type='submit'>Ask</button>
+      </form>
+      <p className='response-area'>
+        { loading ? 'loading...' : response }
+      </p>
+      <div className='footer'>~ webstyle press ~</div>
     </div>
   );
 }
