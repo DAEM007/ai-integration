872e8fd074fe0a248cf776dadfc2a2727f8ec981
DAMILOLA <dammylola299@gmail.com>
2023-11-22 21:01:55 +0100

    updated server

--- src/server.js
+++ src/server.js
@@ -2,4 +2,35 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const { Configuration, OpenAIapi } = require('openai');
+
+// server setup
+const app = express();
+
+// parse body requests
+app.use(bodyParser.json());
+
+// handle cors request
+app.use(cors());
+
+// chatgpt api endpoint setup
+const configuration = new Configuration({
+    apiKey: process.env.CHATBOT_KEY
+});
+
+const openai = new OpenAIapi(configuration);
+
+app.post('/chat', async (req, res) => {
+    const { prompt } = req.body;
+
+    const completion = await openai.createCompletion({
+        model: 'text-curie-001',
+        prompt: prompt,
+        max_tokens: 2049,
+    });
+
+    
+
+});
+
 
